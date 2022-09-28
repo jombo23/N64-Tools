660d9f9a3c8949154d1cac006278e72e718b636f
SubDrag <4073825+SubDrag@users.noreply.github.com>
2022-09-28 18:42:01 -0400

    Missing files

---
+++ N64 Midi Tool/N64MidiLibrary/MKMythologiesDecode.h
@@ -0,0 +1,12 @@
+// BY ZOINKITY
+#pragma once
+
+class CMKMythologiesDecode
+{
+public:
+	CMKMythologiesDecode(void);
+	~CMKMythologiesDecode(void);
+	int  _flags(unsigned char* inputBuffer, int& inputPosition, unsigned char& flags, int& flagLeft);
+	unsigned long helper(unsigned char* inputBuffer, int& inputPosition, unsigned char& flags, int& flagLeft, int num);
+	int dec(unsigned char* inputBuffer, int compressedSize, int dec_s, unsigned char* output);
+};
